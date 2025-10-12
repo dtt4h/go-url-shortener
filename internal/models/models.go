319fa788042b26749be08f1beb83ab5ce1c34543
dtt4h <dtt4h322@gmail.com>
2025-10-12 19:32:01 +0700

    feat: define data models for URL shortening

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,3 +1,12 @@
 // DATA MODELS AND STRUCTURES
 // URL models, req/res DTOs, db entities
 package models
+
+import "time"
+
+type ShortURL struct {
+	ID          string
+	OriginalURL string
+	ShortCode   string
+	CreatedAt   time.Time
+}
