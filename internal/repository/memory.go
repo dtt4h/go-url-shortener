2041f01e5b7e546c41e13e37d8f41cc17280d488
dtt4h <dtt4h322@gmail.com>
2025-10-12 19:33:34 +0700

    feat: add in-memory storage implementation

--- internal/repository/memory.go
+++ internal/repository/memory.go
@@ -1,3 +1,11 @@
 // IN-MEMORY STORAGE IMPLEMENTATION
 // Fast storage for development and testing (no persistance)
 package repository
+
+import (
+	"github.com/dtt4h/go-url-shortener/internal/models"
+)
+
+type MemoryRepo struct {
+	urls map[string]*models.ShortURL
+}
