f2be39eec9caba411c69ff0d7e13d2b7168e0993
dtt4h <dtt4h322@gmail.com>
2025-10-12 19:32:44 +0700

    feat implement short code generation

--- pkg/hash/generator.go
+++ pkg/hash/generator.go
@@ -1,3 +1,17 @@
 // SHORT CODE GENERATION
 // Creates unique short codes from URLs or random generation
 package hash
+
+import (
+	"math/rand"
+)
+
+const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+func GenerateShortCode(length int) string {
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
