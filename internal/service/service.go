167d2a636b1d2cd81b848e7bc0d1f28eaf7679e3
dtt4h <dtt4h322@gmail.com>
2025-10-15 04:30:19 +0700

    feat: define interface

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,3 +1,8 @@
-// SERVICE INTRERFACES
-// Business logic contracts between layers
 package service
+
+import "github.com/dtt4h/go-url-shortener/internal/models"
+
+type Shortener interface {
+	Shorten(url string) (*models.ShortURL, error)
+	Redirect(shortCode string) (string, error)
+}
