f04a5795fa08f284bb2ac99f172b958e7ad5ec90
dtt4h <dtt4h322@gmail.com>
2025-10-15 04:31:08 +0700

    feat: define URLRepository storage interface

--- internal/repository/interface.go
+++ internal/repository/interface.go
@@ -1,3 +1,11 @@
 // STORAGE INTERFACES
 // Contracts for diff storage implementations (memory, postgres)
 package repository
+
+import "github.com/dtt4h/go-url-shortener/internal/models"
+
+type URLRepository interface {
+	Create(url *models.ShortURL) error
+	FindByShortCode(shortCode string) (*models.ShortURL, error)
+	FindByURL(originalURL string) (*models.ShortURL, error)
+}
