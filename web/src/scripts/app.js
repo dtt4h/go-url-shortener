e1ba40129a466841e47eb37b84881da25f9e01a0
emxku <wxnnadxe@mail.ru>
2026-03-10 18:32:44 +0700

    fix: Minor rendering and functional

--- web/src/scripts/app.js
+++ web/src/scripts/app.js
@@ -1,21 +1,34 @@
 const formElement = document.getElementById("mainForm");
 const urlString = document.getElementById("urlString");
 const resultContainer = document.getElementById("resultContainer");
+const copyButton = document.getElementById("copyButton");
+let currentShortUrl;
 
 const urlResult = document.querySelector(".urlResult");
 
 formElement.addEventListener("submit", (evt) => {
   evt.preventDefault();
-  shortResultContainer();
   const newPost = {url: urlString.value}; 
   createShortUrl(newPost);
 })
+
+copyButton.addEventListener("click", copyUrl);
 
 const shortResultContainer = () => {
   resultContainer.classList.add("is-active");
 }
 
-const createShortUrl = (newPost) => {
+async function copyUrl() {
+  try{
+    await navigator.clipboard.writeText(currentShortUrl);
+    console.log("Done!");
+  } catch (err) {
+    console.log("Error by copy", err);
+  }
+}
+
+
+const createShortUrl = (newPost) => { 
   return fetch("http://127.0.0.1:8080/api/v1/shorten", {
       method: "POST",
       body: JSON.stringify(newPost),
@@ -23,6 +36,22 @@ const createShortUrl = (newPost) => {
         "Content-type": "application/json",
       },
     })
-    .then((res) => res.json())
-    .then((data)=> urlResult.textContent = "click.ru/"+(data.short_url.split("/")).pop())//заменить клилкюрл на нашу тему
+    .then((res) => {
+      if (res.ok){
+        return res.json()
+      }
+      else{
+        throw new Error("Failed to get link from server")
+      }
+    })
+    .then((data)=> {
+      currentShortUrl = "click.ru/"+(data.short_url.split("/")).pop();
+      urlResult.textContent = currentShortUrl;
+      shortResultContainer();
+    }) //заменить клилкюрл на нашу тему
+    .catch((err) => {
+      console.log("Create short url failed:", err);
+      currentShortUrl = ""
+      urlResult.textContent = ""
+    })
 }
